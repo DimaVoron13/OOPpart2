0c9cd516c8237f67a171ca3806bc2387b442aaa2
Дмитрий <dima.voron13666@gmail.com>
2024-08-09 06:58:19 +0300

    222

--- src/Diagnostics.java
+++ src/Diagnostics.java
@@ -1,4 +1,5 @@
 public class Diagnostics {
+
     public void check(ServiceStationOfTransport... transports) {
         for (ServiceStationOfTransport transport : transports) {
             transport.repair();
